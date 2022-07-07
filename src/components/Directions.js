e47793fc99935b906104243dd68b8d04fbc08a7e
Ramin Mohammadi <rammoh5346@gmail.com>
2022-07-07 09:06:38 -0500

    Finished Directions page

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -6,11 +6,12 @@ function Directions() {
   const navigate = useNavigate();
   return (
     <>
-      <div>
+      <div className='main'>
         <h2>Directions:</h2>
-        <p>
-          Do this then this
-        </p>
+        <div className="DirectionsText">
+            <p>First you'll choose how many numbers you want to pick from. Then, once you start the game, out of the numbers that will appear on screen, you'll have to try to pick the correct number which is random every time you start a new game.
+            </p>
+        </div>
         <button onClick={() => navigate("/pregame")}>Next</button>
       </div>
     </>
--- src/components/PreGame.js
+++ src/components/PreGame.js
@@ -9,6 +9,7 @@ function PreGame() {
     <>
       <div>
         <h2>Pre-Game:</h2>
+        <p>How many numbers to you want to pick from?</p>
         <p>{num}</p>
         <button className='leftButton' onClick={num-=1}></button>
         <button className='rightButton' onClick={num+=1}></button>
