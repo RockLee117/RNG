242d4fe4ee2682246c13ac94ed48a0d8593bc708
Ramin Mohammadi <rammoh5346@gmail.com>
2022-07-07 08:30:57 -0500

    set up buttons in pregame page

--- src/App.js
+++ src/App.js
@@ -47,14 +47,15 @@ function Directions() {
 }
 
 function PreGame() {
+  let num = 2;
   return(
     <>
       <div>
         <h2>Pre-Game:</h2>
+        <p>{num}</p>
+        <button className='leftButton' onClick={num-=1}></button>
+        <button className='rightButton' onClick={num+=1}></button>
       </div>
-      <nav>
-
-      </nav>
     </>
   );
 }
