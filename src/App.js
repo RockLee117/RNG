14343b060a79d0cc9cc6f9ed947ad046eb918851
Ramin Mohammadi <rammoh5346@gmail.com>
2022-07-07 07:57:19 -0500

    began to set up the Routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<Directions />} />
+        <Route path="about/pregame" element={<PreGame />} />
+      </Routes>
     </div>
   );
 }
 export default App;
 
+function Home() {
+  return (
+    <>
+    <div className="startPage">
+      <h1 className="title">RNG</h1>
+      <div className="playButton">
+        <button type="button">PLAY</button>
+      </div>
+    </div>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function Directions() {
+  return (
+    <>
+      <main>
+        <h2>Directions:</h2>
+        <p>
+          Do this then this
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about/pregame">Next</Link>
+      </nav>
+    </>
+  );
+}
+
+function PreGame() {
+  return(
+    <>
+      <div>
+        <h2>Pre-Game:</h2>
+      </div>
+      <nav>
+        
+      </nav>
+    </>
+  );
+}
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,26 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-const startPage =
-  (
-    <div className="startPage">
-      <h1 className="title">RNG</h1>
-      <div className="playButton">
-        <button type="button">PLAY</button>
-      </div>
-    </div>
-  );
-const main = ReactDOM.createRoot(document.getElementById('main'));
-main.render(startPage);
-
-
-
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
