6fe9b290895e4a2e12e6a662306f6dabad3729ca
Ramin Mohammadi <rammoh5346@gmail.com>
2022-07-07 14:20:45 -0500

    started working on the PlayGame page

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from "react-router-dom";
 import './styles/PlayGame.css';
 
 function PlayGame(){
+    const navigate = useNavigate();
+
+    const [count, setCount] = useState(0);
+    const incrementCount = () => {
+        setCount(count + 1);        
+    };
     return(
         <div>
-            
+            <button>MENU</button>
+            <p>Number of Tries: {count}</p>
         </div>
     );
 }
